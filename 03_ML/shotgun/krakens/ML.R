72dad4340e1f425a7c8eeba376ed4adbfcfb60e6
zhiyu <479321347@qq.com>
2023-09-27 17:44:11 +0800

    zhiyu

--- 03_ML/shotgun/krakens/ML.R
+++ 03_ML/shotgun/krakens/ML.R
@@ -3,23 +3,27 @@ library(phyloseq)
 library(dplyr)
 library(tibble)
 library(ggplot2)
-source("../function/function.R")
+library(microbiome)
+rm(list = ls())
 
 
 
 
 # load("../01_statistical_analysis/Distribution.RData")
-shotgun_phyloseq <- readRDS("../input_raw/shotgun_phyloseq.rds")
-meta_all<-read.csv("../input_raw/meta_ERP012177.csv") %>% data.frame()
-meta_all$Group<-meta_all$config
+shotgun_phyloseq <- readRDS("../../../input_raw/Kraken2_shotgun_phyloseq.rds")
+meta_all<-read.csv("../../../input_raw/meta_PRJDB4176.tsv") %>% data.frame()
+meta_all$host_disease_stat= make.names(meta_all$host_disease_stat)
+meta_all$host_disease_stat=gsub("CRC..","",meta_all$host_disease_stat)
+meta_all$Group<-meta_all$host_disease_stat
 # meta_all$Age<-meta_all$Age_at_collection
 rownames(meta_all)<-meta_all$Run
 
+
 sample_names(shotgun_phyloseq) =gsub(".bracken.mpa.report.*","",sample.names(shotgun_phyloseq))
-sample_data(shotgun_phyloseq)<-meta_all
+sample_data(shotgun_phyloseq)<- meta_all %>% dplyr::filter(Group != "X")
 
 shotgun_phyloseq_core <- microbiome::core(shotgun_phyloseq, detection = 5, prevalence = .2)
-
+Meta=meta(shotgun_phyloseq_core)
 
 ## male
 
@@ -32,7 +36,7 @@ shotgun_phyloseq_core <- microbiome::core(shotgun_phyloseq, detection = 5, preva
 shotgun_phyloseq_core_subset=shotgun_phyloseq_core
 # 查看新的 phyloseq 对象
 shotgun_phyloseq_core_subset
-shotgun_phyloseq_core_subset.tss.level <- microbiome::aggregate_taxa(shotgun_phyloseq_core_subset,level = "Genus") %>%microbiome::transform(transform = "compositional")
+shotgun_phyloseq_core_subset.tss.level <- microbiome::aggregate_taxa(shotgun_phyloseq_core_subset,level = "Genus") %>%microbiome::transform(transform = "compositional") #Genus Species
 OTU_table<-otu_table(shotgun_phyloseq_core_subset.tss.level) %>% t() %>% data.frame()
 meta_phy=sample_data(shotgun_phyloseq_core_subset) %>% data.frame()  %>% dplyr::select(Group)
 OTU_table_Group<-merge(meta_phy,OTU_table,by=0 ) %>% column_to_rownames("Row.names")
@@ -47,10 +51,9 @@ library(DALEX)
 library(forester)
 library(dplyr)
 library(tibble)
+source("../../../function/function.R")
 
-source("../function/function.R")
-
-project_name="ERP012177_"
+project_name="PRJDB4176_"
 ### classfy
 # iris5 <- iris %>% dplyr::filter(Species != "virginica")
 output2 <- forester::train(data = OTU_table_Group,
@@ -83,9 +86,13 @@ draw_Confusion_matrix=forester::draw_confusion_matrix(best_models=output2$models
 pdf(glue::glue(project_name,"best.model_draw_feature_importance_plot.pdf"))
 draw_feature_importance_plot
 dev.off() 
+
+
 pdf(glue::glue(project_name,"best.model_draw_Roc_plot.pdf"))
 draw_Roc_plot
 dev.off() 
+
+
 pdf(glue::glue(project_name,"best.draw_Confusion_matrix.pdf"))
 draw_Confusion_matrix
 dev.off() 
--- input_raw/Readme.txt
+++ input_raw/Readme.txt
@@ -2,3 +2,6 @@
 
 
 /data_bk/zhiyu/pipeline_data/shotgun/ncbi_PRJDB4176/genomes/atlas_binning_phyloseq.rds  # 33site
+
+
+/data_bk/zhiyu/pipeline_data/shotgun/ncbi_PRJDB4176/krean2/phyloseq/Kraken2_shotgun_phyloseq.rds #33site
